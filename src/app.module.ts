548d63a56d8d26ef5cbf831f10ef8a4da97097ea
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-09-07 12:31:52 -0300

    Question creation controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth-module'
 import { AuthenticateController } from './controllers/authenticate.controller'
-import { createQuestionController } from './controllers/create-question.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -18,7 +18,7 @@ import { createQuestionController } from './controllers/create-question.controll
   controllers: [
     CreateAccountController,
     AuthenticateController,
-    createQuestionController,
+    CreateQuestionController,
   ],
   providers: [PrismaService],
 })
