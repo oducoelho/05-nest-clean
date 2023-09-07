cd99507e998287556fe4fdaba71444f7bc28d6b3
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-09-07 10:49:56 -0300

    Protecting Routes with Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth-module'
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { createQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -14,7 +15,11 @@ import { AuthenticateController } from './controllers/authenticate-controller'
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    createQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
