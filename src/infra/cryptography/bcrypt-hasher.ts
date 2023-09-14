3b0eff4ba54134caea1f7bf640ac572638914608
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-09-14 17:10:58 -0300

    Encryption Implementation

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,14 @@
+import { HashCompare } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { compare, hash } from 'bcryptjs'
+
+export class BcryptHasher implements HashGenerator, HashCompare {
+  private HASH_SALT_LENGTH = 8
+  async hash(plain: string): Promise<string> {
+    return hash(plain, this.HASH_SALT_LENGTH)
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash)
+  }
+}
